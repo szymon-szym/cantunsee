55742458bca1d4012121ac066c3a3c030b51fe51
szymon <szymon.mich@gmail.com>
2019-04-07 16:11:12 +0200

    deploy

---
+++ precache-manifest.899a3140d10f0aa1aad4c80ad67ff9db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cantunsee/robots.txt"
+  },
+  {
+    "revision": "d26ad9b6b2d9f23f76dc",
+    "url": "/cantunsee/js/chunk-vendors.5101329d.js"
+  },
+  {
+    "revision": "ca5e651d3c1f891f468d",
+    "url": "/cantunsee/js/app.2ab9cddb.js"
+  },
+  {
+    "revision": "d0efc9417b569bfbe94a30c5318e399a",
+    "url": "/cantunsee/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/cantunsee/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ca5e651d3c1f891f468d",
+    "url": "/cantunsee/css/app.9841dae0.css"
+  }
+];
